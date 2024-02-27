fa2120a3e33370778469e368a176cf25efc176bb
ShivaMoradi <shiva.moradi1@gmail.com>
2024-02-27 11:10:55 +0100

    just changed some text from swedish to english

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -9,15 +9,15 @@ function ContactPage() {
     <main>
       <div class="contact">
         <div class="form-group">
-          <label for="exampleFormControlInput1">Din email</label>
+          <label for="exampleFormControlInput1">your email</label>
           <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="namn@exempel.se" />
         </div>
         <div class="form-group">
-          <label for="exampleFormControlTextarea1">Ditt meddelande</label>
+          <label for="exampleFormControlTextarea1">Your Message</label>
           <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
         <div class="d-flex justify-content-end">
-          <button type="button" onClick={sendMessage} class="btn btn-outline-dark">Skicka ditt meddelande</button>
+          <button type="button" onClick={sendMessage} class="btn btn-outline-dark">Send your message</button>
         </div>
       </div>
     </main>
