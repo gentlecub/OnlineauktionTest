c44259810b405066662fe3d05da0c107853f6462
Joakim <joakim.kvistholm@gmail.com>
2024-02-27 11:15:42 +0100

    add car json file info

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -6,7 +6,7 @@ function MyNav() {
   return (
     <header>
       <nav class="nav flex-column flex-sm-row">
-        <Link to="/" class="flex-sm-fill flex-grow-1 text-sm-left nav-link active"><h1>Lumia</h1></Link>
+        <Link to="/" class="flex-sm-fill flex-grow-1 text-sm-left nav-link active"><h1>Online Auction</h1></Link>
         <Link to="/my-search-input-page" class="flex-sm-fill flex-grow-1 text-sm-left nav-link active">Search wares</Link>
         <Link to="/cart-page" class="flex-sm-fill flex-grow-1 text-sm-left nav-link active">To Cart</Link>
         <Link to="/contact-page" class="text-sm-right nav-link" >Kontakta oss</Link>
