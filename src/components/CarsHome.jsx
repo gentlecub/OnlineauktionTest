012f72629915015b94bb0055e511c473e5f05b11
Joakim <joakim.kvistholm@gmail.com>
2024-02-27 15:09:19 +0100

    Work on home page.

--- src/components/CarsHome.jsx
+++ src/components/CarsHome.jsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 function CarsHome() {
 
   const [cars, setCars] = useState([
-]);
-   
+  ]);
+
   useEffect(() => {
     async function load() {
       const response = await fetch('/api/cars')
@@ -13,12 +13,18 @@ function CarsHome() {
     }
     load()
   }, []) // tom array som andra argument = körs endast vid komponentladdning
-   console.log(cars)
+  console.log(cars)
   return <>
-     <p>Hello</p>
-    {cars.map((car) => (
-      <p>{ car.id }</p>
-    ))}
+    <p>Cars in the auction</p>
+    <ol>
+      { cars.map((car) => <li>
+        <p>{car.brand}</p>
+        <p>{car.model}: {car.year}</p>
+      </li>
+      )}
+    </ol>
+
   </>
 }
-export default  CarsHome 
+
+export default CarsHome 
