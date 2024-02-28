ddda5b7e49e552ea04a8d215560458230413049f
gentlecub <118164637+gentlecub@users.noreply.github.com>
2024-02-28 11:26:42 +0100

    Update CarsHome.jsx

--- src/components/CarsHome.jsx
+++ src/components/CarsHome.jsx
@@ -1,29 +1,49 @@
 import { useEffect, useState } from "react";
 
-function CarsHome () {
+function CarsHome() {
 
-  const [ cars, setCars ] = useState( [
-  ] );
-
-  useEffect( () => {
-    async function load () {
-      const response = await fetch( '/api/cars' )
+  const [cars, setCars] = useState([
+]);
+   
+  useEffect(() => {
+    async function load() {
+      const response = await fetch('/api/cars')
       const cars = await response.json()
-      setCars( cars )
+      setCars(cars)
     }
     load()
-  }, [] ) // tom array som andra argument = körs endast vid komponentladdning
-  console.log( cars )
-  return <>
-    <p>Cars in the auction</p>
-    <ol>
-      { cars.map( ( car ) => <li>
-        <p>{ car.brand }</p>
-        <p>{ car.model }: { car.year }</p>
-      </li>
-      ) }
-    </ol>
-  </>
-}
+  }, []) // tom array som andra argument = körs endast vid komponentladdning
+ const handleSelect = (id) => {
+        const listItems = cars.map((item) => item.id === id );
+        setCars(listItems);
+        console.log(listItems)
+    }
 
-export default CarsHome
+  return (
+      <main>
+            {cars.length ? (
+                <ul className="container">
+                  {cars.map((car) => (
+                    <li className="item" key={car.id}>
+                        <div onClick={() => handleSelect(car.id)}>
+                          <div className="item">
+                            <img src="imagen1.jpg" alt="Imagen 1"/>
+                        </div>
+                        <div className="information">
+                            <p>{ car.brand }</p>
+                            <span className="name">{ car.brand }</span>
+                            <span className="description">{car.color}</span>
+                          </div>
+                        </div>
+                    </li>
+                    ))}
+                </ul>
+            ) : (
+                <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+            )}
+        </main>   
+
+
+    )
+}
+export default  CarsHome 
