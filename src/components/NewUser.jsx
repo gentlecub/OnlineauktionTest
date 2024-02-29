d92737b7600160307117e9094c4445ddee57691c
ShivaMoradi <shiva.moradi1@gmail.com>
2024-02-29 14:36:51 +0100

    created form in registering page for users and also added component to POST new users in json file

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,26 +1,19 @@
-import React, { useEffect, useState } from "react";
 
-function NewUser () {
-  const [ users, setUsers ] = useState( [
-  ] );
+function NewUser (event) {
 
-  useEffect( () => {
-    async function load () {
-      const response = await fetch( '/api/Users' )
-      const users = await response.json()
-      setUsers( users )
-    }
-    load()
-  }, [] )
-  const handleSelect = ( id ) => {
-    const filteredUsers = users.filter( ( item ) => item.id === id )
-    setUsers( filteredUsers )
-    console.log( filteredUsers )
-    
-    return (
-  <h1>Hello</h1>
-)
-  }
+  event.preventDefault()
+  const data = new FormData( event.target )
+  const info = Object.fromEntries( data )
+  fetch( "/api/Users", {
+    method: "POST", // or 'PUT'
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify( info ),
+  } );
+
+
+
 
   }
 export default NewUser
--- src/pages/RegisterUser.jsx
+++ src/pages/RegisterUser.jsx
@@ -1,46 +1,24 @@
 import NewUser from "../components/NewUser.jsx";
 
 function RegisterUser () {
-  function successfullRegister () {
-   
-    return (
-      <NewUser/>
-    )
-  }
+  
+  return (
+    <div>
+      <form onSubmit={NewUser}>
+                <h1>User Registration</h1>
+                <label>Username :</label> <input type="text" name="Username" placeholder="UserName..." /><br />
+        <label>Password :</label> <input type="password" name="password" placeholder="Password..." /><br />
+        <label>Name :</label> <input type="text" name="name" placeholder="Name..." /><br />
+        <label>Email :</label> <input type="text" name="Email" placeholder="exemple@auction.se..." /><br />
+        <label>Roll :</label> <input type="text" name="roll" placeholder="admin or user?..." /><br />
+        <input type="submit" value="Register" />
+            </form>
 
-  return <>
-    <main>
-      <div className="user">
-        <div className="form-group">
-          <label >Id</label>
-          <input type="id" className="form-control" placeholder="id" />
-        </div>
-        <div className="form-group">
-          <label >Username</label>
-          <input type="username" className="form-control"  placeholder="username" />
-        </div>
-        <div className="form-group">
-          <label >Password</label>
-          <input type="password" className="form-control"  placeholder="namn12345" />
-        </div>
-        <div className="form-group">
-          <label >Name</label>
-          <input type="name" className="form-control"  placeholder="fullname" />
-        </div>
-        <div className="form-group">
-          <label >Email</label>
-          <input type="email" className="form-control"  placeholder="namn@exempel.se" />
-        </div>
-        <div className="form-group">
-          <label >Roll</label>
-          <input type="roll" className="form-control"  placeholder="Admin or User?" />
-        </div>
-        <div className="d-flex justify-content-end">
-          <button type="button" onClick={ successfullRegister } className="btn btn-outline-dark">Register</button>
-        </div>
-      </div>
-    </main>
-  </>
+    </div>
+   
+
+  )
+    
 
 }
 export default RegisterUser
