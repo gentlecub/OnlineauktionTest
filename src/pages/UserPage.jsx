60dbe0db3445d797d413db1da04355467481a7a5
desmonddibba <desmonddibba@gmail.com>
2024-03-08 19:36:23 +0100

    Added create auction button

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,20 +1,25 @@
 import AuctionForm from "../components/Auctionform";
+import { useState } from "react";
 
 function UserPage(){
 
     // must be connected to user & should only be visible when user is logged in
 
+    const [showForm, setShowForm] = useState(false);
+
     
-    const handleAuctionSubmit = (auctionData) => {
-        console.log(auctionData)
+    const handleAuctionSubmit = (auctionFormData) => {
+        console.log(auctionFormData + "Was created in the Database.")
+        setShowForm(false);
     }
 
 
-    return(
+    return (
+
         <div>
             <h1>User Dashboard</h1>
-            <AuctionForm onSubmit={handleAuctionSubmit} />
-
+            { showForm ? ( <AuctionForm onSubmit={handleAuctionSubmit} closeForm = {() => setShowForm(false)} />) : 
+            (<button className="btn btn-primary" onClick={ () => setShowForm(true)}> Create new Auction </button> ) }
         </div>
     )
 
