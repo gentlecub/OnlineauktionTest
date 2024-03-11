11f737927f09d5097b689a5fab507b4bc0359176
desmonddibba <desmonddibba@gmail.com>
2024-03-11 20:25:24 +0100

    Moved Login form from /pages to component. Wrote function to check user credentials against db and redirect to userpage/dashboard

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,45 @@
-function Login () {
-  
-  alert("You are loged in")
+import {useNavigate} from 'react-router-dom';
+import {AuthContext} from '../context/AuthContext.jsx';
+import {useContext} from 'react';
 
+
+function Login () {
+
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const username = formData.get('username');
+        const password = formData.get('password');
+
+        const response = await fetch('/api/users');
+        const users = await response.json()
+
+
+        const user = users.find(user => user.username === username && user.password === password);
+
+        if(user) {
+            login(user)
+            console.log(user, 'Logged in successfully.')
+            navigate('/userpage')
+        }else{
+            console.log(user, 'Failed to login')
+        }
+    };
+
+
+    return (
+        <form onSubmit={(handleSubmit)}>
+            <input type="text" name="username" placeholder="Username" />
+            <input type="password" name="password" placeholder="Password" />
+            <input type="submit" value="Login" /> 
+        </form>
+    );
 }
+
+
+
 export default Login
