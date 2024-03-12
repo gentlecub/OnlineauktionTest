13117fd0c47cdafe5711e62a4896a292ba3b2d9f
ShivaMoradi <shiva.moradi1@gmail.com>
2024-03-12 10:28:09 +0100

    no -m

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -11,7 +11,7 @@ function MyNav() {
       <nav className="nav flex-column flex-sm-row">
         <Link to="/" className="text-sm-right nav-link" >Home</Link>
 
-        {user ? (<Link to="/userpage" className="text-sm-right nav-link" >Userpage</Link>) : <Link to="/registering-page" className="text-sm-right nav-link" >Register / Login</Link> } 
+        {user ? (<Link to="/userpage" className="text-sm-right nav-link" >Userpage</Link>) : (<Link to="/registering-page" className="text-sm-right nav-link" >Register / Login</Link>) } 
         
         <Link to="/contact-page" className="text-sm-right nav-link" >Contact Us</Link>
 
