768efd9de019a24def3aa1785c67815416a019bb
desmonddibba <desmonddibba@gmail.com>
2024-03-15 12:55:06 +0100

    Moved code and html to UserDashboard.jsx component

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,49 +1,12 @@
-import AuctionForm from "../components/Auctionform";
-import { useState, useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import UserDashboard from "../components/UserDashboard";
 
 function UserPage () {
 
-    const { logout } = useContext( AuthContext );
-
-    // must be connected to user & should only be visible when user is logged in
-
-    const [ showForm, setShowForm ] = useState( false );
-
-
-    const handleAuctionSubmit = ( auctionFormData ) => {
-        console.log( auctionFormData, "Was created in the Database." )
-        alert('Auction created successfully.')
-        setShowForm( false );
-    }
-
-
-    const handleLogout = () => {
-        alert('Goodbye')
-        logout()
-    }
-
     return (
-        <>
-            <h1>User Dashboard</h1>
 
-
-
-            <div>
-                { showForm ? ( <AuctionForm onSubmit={ handleAuctionSubmit } closeForm={ () => setShowForm( false ) } /> ) :
-                    ( <button className="btn btn-primary" onClick={ () => setShowForm( true ) }> Create new Auction </button> ) }
-            </div>
-
-
-            <div>
-                <br></br>
-                <button onClick={ handleLogout } className="btn btn-primary" >Logout</button>
-            </div>
-
-
-        </>
+        <UserDashboard />
     )
-
+  
 }
 
 
