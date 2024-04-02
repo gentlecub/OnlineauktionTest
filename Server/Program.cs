e3f420082424de9e8b1c70d7eccf2a1b07938f0d
ShivaMoradi <shiva.moradi1@gmail.com>
2024-04-02 14:42:06 +0200

    my sql connection code in program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,41 @@
+using MySql.Data.MySqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+MySqlConnection? db = null;
+string connectionString = "server=localhost;uid=root;pwd=mypassword;database=onlineauction;port=3306";
+
+try
+{
+
+    db = new(connectionString);
+    db.Open();
+
+    builder.Services.AddSingleton(new State(db));
+    var app = builder.Build();
+
+
+   
+    
+
+    app.MapGet("/", () => "Hello World!");
+
+
+
+    app.Run();
+
+
+}
+catch (MySqlException e)
+{
+    Console.WriteLine(e);
+
+}
+finally
+{
+    db.Close();
+}
+
+
+public record State(MySqlConnection DB);
