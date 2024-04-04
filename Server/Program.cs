c028730b7e689be877e526d70c29c70d581df911
Joakim <joakim.kvistholm@gmail.com>
2024-04-04 12:08:11 +0200

    Feature Epcommon: Fixat admin, user och not logged in check with cookies.

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,7 +2,9 @@ using MySql.Data.MySqlClient;
 using Onlineauction;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddAuthentication().AddCookie("opa23.onlineauction.cars");
+builder.Services.AddAuthorizationBuilder().AddPolicy("admin_route", policy => policy.RequireRole("admin"));
+builder.Services.AddAuthorizationBuilder().AddPolicy("user_route", policy => policy.RequireRole("user"));
 
 MySqlConnection? db = null;
 string connectionString = "server=localhost;uid=root;pwd=mypassword;database=onlineauction;port=3306";
@@ -16,9 +18,11 @@ try
     builder.Services.AddSingleton(new State(db));
     var app = builder.Build();
 
+    app.MapPost("/login", Auth.Login);
+    app.MapGet("/admin", () => "Hello, Admin!").RequireAuthorization("admin_route");
+    app.MapGet("/user", () => "Hello, User!").RequireAuthorization("user_route");
 
-   
-    
+
 
     app.MapGet("/", () => "Hello World!");
     app.MapGet("/users", Users.All);
