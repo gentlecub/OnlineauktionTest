9d9ca51303f6c6c2ef0f145ab18ac83bc55b47b5
desmonddibba <desmonddibba@gmail.com>
2024-05-06 13:58:32 +0200

    Refactored Cypress tests with custom command. Added custom commands in cypress.cofig

--- cypress/specs/step_definitions/navigationTest.js
+++ cypress/specs/step_definitions/navigationTest.js
@@ -3,16 +3,15 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 Given('I am on the homepage and the navbar is visible', () => {
   cy.visit('/');
   cy.location("pathname").should("equal", "/");
-  cy.get('[data-test="nav"]').should("be.visible")
-
+  cy.getDataTest('nav').should("be.visible")
 });
 
 Then('the navbar should not contain userpage', () => {
-  cy.get('[data-test="nav"]').should("not.contain", /userpage/i)
+  cy.getDataTest('nav').should("not.contain", /userpage/i)
 });
 
 When('I click on Contact Us in the navbar', () => {
-  cy.get('[data-test="nav-contact-page"]').click();
+  cy.getDataTest('nav-contact-page').click();
 });
 
 Then('the contact page should be displayed', () => {
@@ -21,7 +20,7 @@ Then('the contact page should be displayed', () => {
 });
 
 When('I click on Show Current Auction in the navbar', () => {
-  cy.get('[data-test="nav-show-auction-page"]').click();
+  cy.getDataTest('nav-show-auction-page').click();
 });
 
 Then('the show auction page should be displayed', () => {
@@ -29,7 +28,7 @@ Then('the show auction page should be displayed', () => {
 });
 
 When('I click on Register Login in the navbar', () => {
-  cy.get('[data-test="nav-registering-page"]').click();
+  cy.getDataTest('nav-registering-page').click();
 });
 
 Then('the registration page should be displayed', () => {
@@ -39,7 +38,7 @@ Then('the registration page should be displayed', () => {
 });
 
 When('I click on Home in the navbar', () => {
-  cy.get('[data-test="nav-home"]').click();
+  cy.getDataTest('nav-home').click();
 });
 
 Then('the homepage should be displayed', () => {
